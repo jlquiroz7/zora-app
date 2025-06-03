3f9cbebcecb5537c35beac13fd87351465489a93
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-06-03 15:29:05 -0700

    Version name implementado

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Chat from "@/app/components/chat";
+import { VERSION_NAME } from "./local.json";
 
 export default function Home() {
   return (
-    <Chat />
+    <div className="flex flex-col">
+      <p className="text-center bg-blue-100">{VERSION_NAME}</p>
+      <Chat />
+    </div>
   );
 }
